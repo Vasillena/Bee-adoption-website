9b1cb3845bf70ccacc4431cacf2204c51cb85d54
Vasilena Yoneva <114015792+Vasillena@users.noreply.github.com>
2023-06-20 15:48:34 +0300

    Update util.js

--- src/data/util.js
+++ src/data/util.js
@@ -22,16 +22,3 @@ export function createSubmitHandler(callback) {
     callback(data, form);
   };
 }
-
-// export function createSubmitHandler(callback) {
-//   return function (event) {
-//     event.preventDefault();
-//     const form = event.target;
-//     const formData = new FormData(form);
-//     const data = Object.fromEntries(formData.entries());
-
-//     if (typeof callback === "function") {
-//       callback(data, form);
-//     }
-//   };
-// }
